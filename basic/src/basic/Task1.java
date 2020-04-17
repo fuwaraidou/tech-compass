1cc25e566f6e3b9c22d13d7663076aa643a1bd9a
wade <wade@colla-tech.co.jp>
2020-04-17 10:46:21 +0900

    レビュー指摘の修正

--- basic/src/basic/Task1.java
+++ basic/src/basic/Task1.java
@@ -4,13 +4,15 @@ public class Task1 {
 
 	public static void main(String[] args) {
 		// 課題の解答をコーディング
-		System.out.println("test");
-
-		int a = 0;
+		int sum = 0;
 		for (int i=0; i<2; i++) {
-		if (i != 0) a = a + 1;
+			if (i == 0) {
+				sum = sum + 1;
+			} else {
+				sum = sum + 2;
 			}
-		System.out.println("合計：" + a);
+		}
+		System.out.println("合計：" + sum);
 	}
 
 }
