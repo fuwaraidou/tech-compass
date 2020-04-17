6cfacbc4611318cf2507e0ed0996ee0d73ad8696
wade <wade@colla-tech.co.jp>
2020-04-17 09:54:33 +0900

    ブランチ切り替え

--- basic/src/basic/Task1.java
+++ basic/src/basic/Task1.java
@@ -5,6 +5,12 @@ public class Task1 {
 	public static void main(String[] args) {
 		// 課題の解答をコーディング
 		System.out.println("test");
+
+		int a = 0;
+		for (int i=0; i<2; i++) {
+		if (i != 0) a = a + 1;
+			}
+		System.out.println("合計：" + a);
 	}
 
 }
